43bbec389115eec26fb65388ccd5b88618b12451
Lattice23 <Tubbylettice@gmail.com>
2025-05-30 11:51:04 -0400

    update

--- exegol/load_user_setup.sh
+++ exegol/load_user_setup.sh
@@ -6,14 +6,50 @@ set -e
 # It is strongly advised **not** to overwrite the configuration files provided by exegol (e.g. /root/.zshrc, /opt/.exegol_aliases, ...), official updates will not be applied otherwise.
 #
 # --- INSTALL YAZI ----
+
 install_yazi() {
+  echo "Installing Yazi"
 
   sudo apt update
-  apt install -y ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide imagemagick
+  apt install -y ffmpeg 7zip jq poppler-utils fd-find ripgrep zoxide imagemagick
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   cargo install --locked yazi-fm yazi-cli
-  mkdir /root/.config/yazi/
-  curl -o /root/.config/yazi/yazi.toml https://raw.githubusercontent.com/sxyazi/yazi/refs/heads/main/yazi-config/preset/yazi-default.toml
+  cp -r /opt/my-resources/setup/yazi/ /root/.config/
+
+  echo "Yazi installed"
 }
+
+install_starship() {
+  cargo install eza starship
+
+  cp /opt/my-resources/setup/starship/starship.toml /root/.config/
+  cp /opt/my-resources/setup/zsh/zsh.bak /root/.zshrc
+
+  echo "starship and eza installed"
+}
+
+install_nu() {
+  echo "Installing nushell"
+
+  curl -fsSL https://apt.fury.io/nushell/gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/fury-nushell.gpg
+  echo "deb https://apt.fury.io/nushell/ /" | sudo tee /etc/apt/sources.list.d/fury.list
+  sudo apt update --allow-insecure-repositories
+  yes | sudo apt install nushell
+
+  cp /opt/my-resources/setup/nushell/config.nu /root/.config/nushell/
+  echo "Nushell installed"
+}
+
+install_yazi
+sleep 2
+install_starship
+sleep 2
+install_nu
+
+zsh
+echo -e "\n DONE :)\n"
+
+# ---- LOAD TMUX CONF ----
 
 # Exegol also features a set of supported customization a user can make.
 # The /opt/supported_setups.md file lists the supported configurations that can be made easily.
