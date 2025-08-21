933557dbf75bde0a4901e174cf6abd072c389e78
Lattice23 <Tubbylettice@gmail.com>
2025-08-20 22:22:43 -0400

    exegol stuff

--- exegol/bin/Exegol-history/exegol-history.py
+++
@@ -1,5 +0,0 @@
-#!/opt/tools/Exegol-history/venv/bin/python3
-from exegol_history.__main__ import main
-
-if __name__ == "__main__":
-    main()
--- exegol/bin/Exegol-history/profile.sh
+++
@@ -1,5 +0,0 @@
-export USER='who'
-export PASSWORD='bro'
-export NT_HASH=''
-export DOMAIN='ii.ola'
-echo "$TARGET" > /tmp/target_value.tmp
